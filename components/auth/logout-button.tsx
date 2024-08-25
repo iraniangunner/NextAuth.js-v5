c30921a8f7ef9db1537467e631c98b121b40f0e4
iraniangunner <fateh19950@hotmail.com>
2024-08-25 21:47:34 +0330

    add client signout button

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { DropdownItem } from "flowbite-react";
 import { logout } from "@/actions/logout";
+import { signOut } from "next-auth/react";
 
 export default function LogoutButton() {
-  const onClick = () => logout();
+  // const onClick = () => logout();
+  const onClick = () => signOut();
   return <DropdownItem onClick={onClick}>Sign Out</DropdownItem>;
 }
