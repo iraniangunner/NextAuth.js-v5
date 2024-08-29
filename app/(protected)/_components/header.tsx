e18ca1ad69d6c5056267339c73777b3951a8ce4e
iraniangunner <fateh19950@hotmail.com>
2024-08-29 21:20:22 +0330

    project almost completed!

--- app/(protected)/_components/header.tsx
+++ app/(protected)/_components/header.tsx
@@ -3,8 +3,8 @@ import { UserButton } from "@/components/auth/user-button";
 
 export const DashboardHeader = () => {
   return (
-    <nav className="bg-secondary flex items-center justify-end p-4 rounded-xl w-[600px] shadow-sm">
-      {/* <DashboardNavbar/> */}
+    <nav className="bg-secondary flex items-center justify-between p-4 rounded-xl w-[600px] shadow-sm">
+      <DashboardNavbar/>
       <UserButton />
     </nav>
   );
--- components/auth/dashboard-navbar.tsx
+++ components/auth/dashboard-navbar.tsx
@@ -2,32 +2,37 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { FaRegArrowAltCircleLeft } from "react-icons/fa";
 
 export const DashboardNavbar = () => {
   const pathname = usePathname();
   return (
     <div className="flex gap-x-2">
-      <Button asChild variant={pathname === "/server" ? "default" : "outline"}>
-        <Link href="/dashboard">Dashboard</Link>
+      <Button asChild variant="outline">
+        <Link href="/">
+          <FaRegArrowAltCircleLeft className="h-4 w-4 mr-2" />
+          Back to Home
+        </Link>
       </Button>
-      {/* <Button
-          asChild
-          variant={pathname === "/client" ? "default" : "outline"}
-        >
-          <Link href="/client">Client</Link>
-        </Button> */}
-      {/* <Button asChild variant={pathname === "/admin" ? "default" : "outline"}>
-        <Link href="/admin">Admin</Link>
-      </Button> */}
+      <Button
+        asChild
+        variant={pathname === "/dashboard" ? "default" : "outline"}
+      >
+        <Link href="/dashboard">
+          <MdOutlineDashboardCustomize className="h-4 w-4 mr-2" />
+          Dashboard
+        </Link>
+      </Button>
       <Button
         asChild
         variant={pathname === "/settings" ? "default" : "outline"}
       >
-        <Link href="/settings">Settings</Link>
-      </Button>
-
-      <Button asChild variant={pathname === "/" ? "default" : "outline"}>
-        <Link href="/">main</Link>
+        <Link href="/settings">
+          <IoSettingsOutline className="h-4 w-4 mr-2" />
+          Settings
+        </Link>
       </Button>
     </div>
   );
