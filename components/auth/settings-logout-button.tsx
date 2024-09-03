3973a6d2dda2610e39591534e6f7aeecf99b674e
iraniangunner <fateh19950@hotmail.com>
2024-09-04 02:47:39 +0330

    some changes

--- components/auth/settings-logout-button.tsx
+++ components/auth/settings-logout-button.tsx
@@ -1,12 +1,15 @@
 "use client";
-import { logout } from "@/actions/logout";
+// import { logout } from "@/actions/logout";
+
+import {signOut} from "next-auth/react"
+
 
 export const SettingsLogoutButton = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const onClick = () => logout();
+  const onClick = () => signOut();
 
   return (
     <span onClick={onClick} className="cursor-pointer">
