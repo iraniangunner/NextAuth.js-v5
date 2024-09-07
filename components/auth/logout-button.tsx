8622f2700eb5118838fb700be2408f70241e3be4
fateh moonesi <f.moonesi@jsk.local>
2024-09-07 15:54:21 +0330

    onClick added

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -4,7 +4,7 @@ import { logout } from "@/actions/logout";
 import { signOut } from "next-auth/react";
 
 export default function LogoutButton() {
-  // const onClick = () => logout();
-  const onClick = () => signOut({callbackUrl:"/auth/login"});
+  const onClick = () => logout();
+  // const onClick = () => signOut({callbackUrl:"/auth/login"});
   return <button onClick={onClick}>Sign Out</button>;
 }
