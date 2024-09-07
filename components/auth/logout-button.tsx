1309b9c5d535c0a44c85aec91d7a04d317b3c098
fateh moonesi <f.moonesi@jsk.local>
2024-09-07 15:42:48 +0330

    onClick added to button

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -4,7 +4,7 @@ import { logout } from "@/actions/logout";
 import { signOut } from "next-auth/react";
 
 export default function LogoutButton() {
-  const onClick = () => logout();
-  // const onClick = () => signOut();
+  // const onClick = () => logout();
+  const onClick = () => signOut({callbackUrl:"/"});
   return <DropdownItem onClick={onClick}>Sign Out</DropdownItem>;
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+//   async headers() {
+//     return [
+//       {
+//         source: "/api/auth/signout",
+//         headers: [
+//           {
+//             key: "Cache-Control",
+//             value: "no-store",
+//           },
+//         ],
+//       },
+//     ];
+//   },
 };
 
 export default nextConfig;
