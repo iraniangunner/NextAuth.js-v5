eb5bf024d2158e69a8e83e1b5c79fa4fafe0323e
fateh moonesi <f.moonesi@jsk.local>
2024-09-08 12:42:42 +0330

    some changes in login

--- actions/logout.ts
+++ actions/logout.ts
@@ -4,6 +4,6 @@ import { signOut } from "@/auth";
 export const logout = async (formData:FormData) => {
   //some server stuff
   await signOut({
-    redirectTo: "/auth/signin",
+    redirectTo: "/auth/login",
   });
 };
