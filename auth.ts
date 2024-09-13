b1b4a4d7b30e18ed77b64a31a3561eb720e0f295
iraniangunner <fateh19950@hotmail.com>
2024-09-14 00:49:38 +0330

    implement refresh token strategy to google provider

--- auth.ts
+++ auth.ts
@@ -22,7 +22,6 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     },
   },
   callbacks: {
-
     async signIn({ user, account }) {
       //Allow OAuth without email verification
       if (account?.provider !== "credentials") {
@@ -53,7 +52,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       }
       return true;
     },
-    async session({ token, session }) {
+    async session({ token, session, user }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
@@ -67,10 +66,19 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         session.user.isOAuth = token.isOAuth as boolean;
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
+
+      // return session
+      // const timestamp = 1726264153;
+      // const date = new Date(timestamp * 1000);
+      // const hours = date.getHours();
+      // console.log(hours); // Output: 9
+
+      // console.log(token);
+
       return session;
     },
 
-    async jwt({ token, trigger, session }) {
+    async jwt({ token, trigger, session, account }) {
       if (trigger === "update" && session) {
         token = { ...token, ...session };
       }
@@ -89,7 +97,64 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
-      return token;
+      if (account) {
+        // First-time login, save the `access_token`, its expiry and the `refresh_token`
+        return {
+          ...token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          refresh_token: account.refresh_token,
+        };
+      } else if (
+        token.expires_at &&
+        Date.now() < Number(token.expires_at) * 1000
+      ) {
+        // Subsequent logins, but the `access_token` is still valid
+        return token;
+      } else {
+        // Subsequent logins, but the `access_token` has expired, try to refresh it
+        if (!token.refresh_token) throw new TypeError("Missing refresh_token");
+
+        try {
+          // The `token_endpoint` can be found in the provider's documentation. Or if they support OIDC,
+          // at their `/.well-known/openid-configuration` endpoint.
+          // i.e. https://accounts.google.com/.well-known/openid-configuration
+          const response = await fetch("https://oauth2.googleapis.com/token", {
+            method: "POST",
+            body: new URLSearchParams({
+              client_id: process.env.GOOGLE_CLIENT_ID!,
+              client_secret: process.env.GOOGLE_CLIENT_SECRET!,
+              grant_type: "refresh_token",
+              refresh_token: token.refresh_token!,
+            } as any),
+          });
+
+          const tokensOrError = await response.json();
+
+          if (!response.ok) throw tokensOrError;
+
+          const newTokens = tokensOrError as {
+            access_token: string;
+            expires_in: number;
+            refresh_token?: string;
+          };
+
+          token.access_token = newTokens.access_token;
+          token.expires_at = Math.floor(
+            Date.now() / 1000 + newTokens.expires_in
+          );
+          // Some providers only issue refresh tokens once, so preserve if we did not get a new one
+          if (newTokens.refresh_token)
+            token.refresh_token = newTokens.refresh_token;
+          return token;
+        } catch (error) {
+          console.error("Error refreshing access_token", error);
+          // If we fail to refresh the token, return an error so we can handle it on the page
+          token.error = "RefreshTokenError";
+        }
+
+        return token;
+      }
     },
   },
   session: { strategy: "jwt" },
